762c33dc4889b598c86abe3e1b27e0d832c36f7a
tlow22 <32314421+tlow22@users.noreply.github.com>
2019-12-26 14:12:30 -0700

    modify unit tests

--- test/euler_angles_test.jl
+++ test/euler_angles_test.jl
@@ -1,15 +1,10 @@
 @testset "euler_angles" begin
-  # test standard convention
-  eul = EulerAngles(0.1*π, 0.5, 0.25*π)
+  data = (0.1*π, 0.5, 0.25*π)
+  eul = EulerAngles(data...)
+
   @test eul.properties == Bunge()
-  @test eul.data == (0.1*π, 0.5, 0.25*π)
+  @test eul.data == data
   @test eul[1] == 0.1*π
   @test eul[2] == 0.5
   @test eul[3] == 0.25*π
-
-  # test different conventions
-  conventions = [Bunge, Kocks, Matthies, Roe]
-  for T in conventions
-    @test EulerAngles(T, 0.0, 0.0, 0.0).properties == T()
-  end
 end
