14fe23d91bf0d22681bc54116ab11cabb6b63fa8
Thaddeus Low <thaddeuslow@outlook.com>
2020-03-29 15:09:44 -0600

    add isapprox and isequal to EulerAngle types

--- test/euler_angles_test.jl
+++ test/euler_angles_test.jl
@@ -1,10 +1,9 @@
 @testset "euler_angles" begin
-  data = (0.1*π, 0.5, 0.25*π)
-  eul = EulerAngles(data...)
+    data = (0.1*π, 0.5, 0.25*π)
+    eul = EulerAngles(data...)
 
-  @test eul.properties == Bunge()
-  @test eul.data == data
-  @test eul[1] == 0.1*π
-  @test eul[2] == 0.5
-  @test eul[3] == 0.25*π
+    @test eul.data == data
+    @test eul[1] == 0.1*π
+    @test eul[2] == 0.5
+    @test eul[3] == 0.25*π
 end
