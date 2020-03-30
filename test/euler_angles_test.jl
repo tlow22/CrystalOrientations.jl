7ed01947c0fb984aa7b9b057aa2d4e93629c5b70
Thaddeus Low <thaddeuslow@outlook.com>
2020-03-29 19:19:44 -0600

    test EulerAngle boolean operators

--- test/euler_angles_test.jl
+++ test/euler_angles_test.jl
@@ -1,9 +1,13 @@
 @testset "euler_angles" begin
     data = (0.1*π, 0.5, 0.25*π)
-    eul = EulerAngles(data...)
+    eul  = EulerAngles(data...)
 
+    @test eul[1] == data[1]
+    @test eul[2] == data[2]
+    @test eul[3] == data[3]
     @test eul.data == data
-    @test eul[1] == 0.1*π
-    @test eul[2] == 0.5
-    @test eul[3] == 0.25*π
+    @test eul ≈  EulerAngles(Bunge, 0.1*π, 0.5, 0.25*π)
+    @test eul == EulerAngles(Bunge, 0.1*π, 0.5, 0.25*π)
+    @test eul != EulerAngles(Bunge, 0.0, 0.0, 0.0)
+    @test eul != EulerAngles(Kocks, 0.1*π, 0.5, 0.25*π)
 end
